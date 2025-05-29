ed465a69ec2bf16307c812b7ff8730a090f4eed9
casqade <ivamspasibo@gmail.com>
2025-05-30 01:09:51 +0300

    upd: moved patch API related functions to separate file

--- Patch.cpp
+++ Patch.cpp
@@ -1,34 +1,10 @@
-#include <windows.h>
-#include <iostream>
-
-//-----------------------------------------------------------------------------
-VOID EraseCode(DWORD destAddress, BYTE nopCount);
-VOID InjectCode(DWORD destAddress, VOID(*func)(VOID), BYTE nopCount);
-VOID WriteBytesASM(DWORD destAddress, LPVOID patch, DWORD numBytes);
-
-#if defined(PATCH_ENABLE_CONSOLE)
-VOID CreateConsole();
-#endif
-
-//-----------------------------------------------------------------------------
-
-DWORD currentTime;
-DWORD previousTime;
-
-//-----------------------------------------------------------------------------
-
-BOOL  gameSpeedLockEnabled = TRUE;
-CONST FLOAT gameSpeedRate = 30.0f;
-CONST DWORD gameSpeedDelta = 1000.0f / gameSpeedRate;
-
-//-----------------------------------------------------------------------------
+#include "PatchAPI.hpp"
 
 
 DWORD returnAddr = 0;
 
-DWORD gameProcId = 0;
-DWORD windowInFocusProcId = 0;
 
+/// Patch addresses
 DWORD injectAddr_MissionInit  = 0x413ACA;
 DWORD injectAddr_SuspendCheck = 0x4045BE;
 
@@ -108,6 +84,16 @@ DWORD func_CancelLanding = 0x515E50;
 
 
 /// DLL variables
+DWORD gameProcId = 0;
+DWORD windowInFocusProcId = 0;
+
+DWORD currentTime;
+DWORD previousTime;
+
+BOOL gameSpeedLockEnabled = TRUE;
+CONST FLOAT gameSpeedRate = 30.0f;
+CONST DWORD gameSpeedDelta = 1000.0f / gameSpeedRate;
+
 DWORD iffOn   = 0;
 DWORD iffPrev = 0;
 DWORD emconOn = 0;
@@ -701,8 +687,7 @@ __declspec(naked) void SuspendIfInactive(void)
 }
 
 
-// Initialize function called by the loader's inject function
-extern "C" __declspec(dllexport) void Initialize()
+void PatchInit()
 {
   InjectCode(injectAddr_MissionInit,  MissionInit, 0);
   InjectCode(injectAddr_SuspendCheck, SuspendIfInactive, 0);
@@ -754,135 +739,4 @@ extern "C" __declspec(dllexport) void Initialize()
 
 
   gameProcId = GetCurrentProcessId();
-
-#if defined(PATCH_ENABLE_CONSOLE)
-  CreateConsole();
-#endif
 }
-
-//-----------------------------------------------------------------------------
-
-// Define the plugins main, use a define since the code is the same for all plugins
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReason, LPVOID lpReserved)
-{
-  // Get rid of compiler warnings since we do not use this parameter
-  UNREFERENCED_PARAMETER(lpReserved);
-
-  // If we are attaching to a process, we do not want the dll thread messages
-  if (ulReason == DLL_PROCESS_ATTACH)
-    DisableThreadLibraryCalls(hModule);
-
-  // Always load/unload
-  return TRUE;
-}
-
-//-----------------------------------------------------------------------------
-
-// Writes bytes in the current process using an ASM method
-VOID WriteBytesASM(DWORD destAddress, LPVOID patch, DWORD numBytes)
-{
-  if ( numBytes < 1 )
-    return;
-
-
-  // Store old protection of the memory page
-  DWORD oldProtect = 0;
-
-  // Store the source address
-  DWORD srcAddress = PtrToUlong(patch);
-
-  // Make sure page is writeable
-  VirtualProtect((void*)(destAddress), numBytes, PAGE_EXECUTE_READWRITE, &oldProtect);
-
-  // Do the patch (oldschool style to avoid memcpy)
-  __asm
-  {
-    nop           // Filler
-    nop           // Filler
-    nop           // Filler
-
-    mov esi, srcAddress   // Save the address
-    mov edi, destAddress  // Save the destination address
-    mov ecx, numBytes   // Save the size of the patch
-
-    Start :
-    cmp ecx, 0        // Are we done yet?
-      jz Exit         // If so, go to end of function
-
-      mov al, [esi]     // Move the byte at the patch into AL
-      mov[edi], al      // Move AL into the destination byte
-      dec ecx         // 1 less byte to patch
-      inc esi         // Next source byte
-      inc edi         // Next destination byte
-      jmp Start       // Repeat the process
-
-    Exit :
-      nop           // Filler
-      nop           // Filler
-      nop           // Filler
-  }
-
-  // Restore old page protection
-  VirtualProtect((void*)(destAddress), numBytes, oldProtect, &oldProtect);
-}
-
-//-----------------------------------------------------------------------------
-
-VOID EraseCode(DWORD destAddress, BYTE nopCount)
-{
-  if ( nopCount < 1 )
-    return;
-
-  // Buffer of NOPs, static since we limit to 'UCHAR_MAX' NOPs
-  BYTE nopPatch[0xFF] = { 0 };
-
-  // Fill it with nops
-  memset(nopPatch, 0x90, nopCount);
-
-  // Make the patch now
-  WriteBytesASM(destAddress, nopPatch, nopCount);
-}
-
-// Codecave function
-VOID InjectCode(DWORD destAddress, VOID(*func)(VOID), BYTE nopCount)
-{
-  // Calculate the code cave
-  DWORD offset = (PtrToUlong(func) - destAddress) - 5;
-
-  // Construct the patch to the function call
-  BYTE patch[5] = { 0xE8, 0x00, 0x00, 0x00, 0x00 };
-  memcpy(patch + 1, &offset, sizeof(DWORD));
-  WriteBytesASM(destAddress, patch, 5);
-
-  EraseCode(destAddress + 5, nopCount);
-}
-
-//-----------------------------------------------------------------------------
-
-#if defined(PATCH_ENABLE_CONSOLE)
-VOID CreateConsole()
-{
-  int hConHandle = 0;
-  HANDLE lStdHandle = 0;
-  FILE *fp = 0;
-
-  AllocConsole();
-
-  FILE* fDummy;
-  freopen_s(&fDummy, "CONIN$", "r", stdin);
-  freopen_s(&fDummy, "CONOUT$", "w", stderr);
-  freopen_s(&fDummy, "CONOUT$", "w", stdout);
-
-  // Note that there is no CONERR$ file
-  HANDLE hStdout = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
-    NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-  HANDLE hStdin = CreateFile("CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
-    NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-  SetStdHandle(STD_OUTPUT_HANDLE, hStdout);
-  SetStdHandle(STD_ERROR_HANDLE, hStdout);
-  SetStdHandle(STD_INPUT_HANDLE, hStdin);
-}
-#endif
-
-//-----------------------------------------------------------------------------
